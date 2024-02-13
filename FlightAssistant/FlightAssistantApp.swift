93c1d855c52870f94255031e3eb5763c3ab3aa54
Joao Duarte <joaoricardoduarte@gmail.com>
2024-02-13 14:36:22 +0000

    Initial Commit

---
+++ FlightAssistant/FlightAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightAssistantApp.swift
+//  FlightAssistant
+//
+//  Created by Joao Duarte on 13/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlightAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
